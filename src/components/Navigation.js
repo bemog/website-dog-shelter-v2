95592c886a005260fd7eae55bdd31e988747aed6
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-07-31 21:45:37 +0200

    Create Navigation component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const links = [
+  { name: 'Start', path: '/', icon: 'fas fa-home fa-2x', exact: true },
+  { name: 'O nas', path: '/about', icon: 'fas fa-users fa-2x' },
+  { name: 'Adopcja', path: '/adoption', icon: 'fas fa-paw fa-2x' },
+  { name: 'Wolontariat', path: '/volunteering', icon: 'fas fa-heart fa-2x' },
+  { name: 'Kontakt', path: '/contact', icon: 'fas fa-envelope fa-2x' },
+]
 
 const Navigation = () => {
-    return (
-        <div>
-            Navigation
-        </div>
-    );
+  const items = links.map(item => (
+    <li key={item.name} className="header__navigation-list-item">
+      <NavLink to={item.path} className="header__navigation-list-item-link">
+        <i className={item.icon}></i>
+        <span className="header__navigation-list-item-title">{item.name}</span>
+      </NavLink>
+    </li>
+  ))
+
+  return (
+    <nav className="header__navigation">
+      <ul className="header__navigation-list">
+        {items}
+      </ul>
+    </nav>
+  );
 }
 
 export default Navigation;
+
