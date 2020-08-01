a4007a05e450374ce4252116cd9d1652427ecea3
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-08-01 16:54:59 +0200

    Add HomePage structure and styling

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import '../styles/HomePage.scss';
+import dog from '../images/dog.png';
+import dogTail from '../images/dogtail.png';
 
 const HomePage = () => {
-    return (
-        <div>
-            HomePage
+  return (
+    <div className="home">
+      <div className="home__welcome">
+        <div className="home__welcome-dogtail">
+          <img className="home__welcome-dogtail-image" src={dogTail} alt="Ogon psa" />
         </div>
-    );
+        <div className="home__welcome-dog">
+          <img className="home__welcome-dog-image" src={dog} alt="Zdjęcie psa" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
